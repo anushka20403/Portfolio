9318aa05ffbeb977d1932e7dbfa44ce3a1571893
Anushka Mohan <90108236+anushka20403@users.noreply.github.com>
2024-08-11 14:36:44 +0530

    Update Work.jsx

--- src/component/Work.jsx
+++ src/component/Work.jsx
@@ -15,12 +15,7 @@ const Work = () => {
         <div className='right_col2'>
         <h2 className='wh2'>Jio Platforms Limited</h2>
         <h3 className='wh3'>May 27th - July 19th 2024, Navi Mumbai</h3>
-        <p className='wp'>As an intern at Jio Platforms Ltd, I performed the following tasks: </p>
-       <p> • Conducted exploratory data analysis on the Jio recharges dataset to identify patterns and trends, and created its
-API using FastAPI.</p>
-<p>• Performed time series forecasting using ARIMA and SARIMAX models.</p>
-<p>• Completed a sentiment analysis project.</p>
-<p>• Developed a small language model project using Google Gemini for automated blog writing.</p>    
+        <p className='wp'>As an intern at Jio Platforms Ltd, I conducted exploratory data analysis on the Jio recharges dataset, created its API using FastAPI, and performed time series forecasting with ARIMA and SARIMAX models. Additionally, I completed a sentiment analysis project and developed a small language model using Google Gemini for automated blog writing. </p>
     </div>
     </div>
     </div>
